34d71b337cdbb208730b106f0b3bee1470cc67d5
Erdem Ersoy <erdem.ersoy@pardus.org.tr>
2020-04-24 12:00:16 +0300

    Oops! 2

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from pardusflatpakstore.version import Version
+from pardusflatpakgui.version import Version
 
 from setuptools import setup, find_packages
 from os import listdir
